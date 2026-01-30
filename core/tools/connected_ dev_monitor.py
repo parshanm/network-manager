bb1fbf34b5c519e629bd4a115081e37636119b51
parshanm <parshan.m.m@gmail.com>
2026-01-30 16:21:08 +0330

    getting access to Adminstrator in windows has been developed.

---
+++ core/tools/connected_ dev_monitor.py
@@ -0,0 +1,3 @@
+from scapy.all import ARP, Ether, srp, conf
+
+
---
+++ core/tools/get_admin_acess.py
@@ -0,0 +1,18 @@
+import elevate
+import ctypes
+
+def check_admin(sed_request:bool):
+    if sed_request:
+        if ctypes.windll.shell32.IsUserAnAdmin():
+            return True
+        else:
+            try:
+                elevate.elevate(graphical=True)
+                if ctypes.windll.shell32.IsUserAnAdmin():
+                    return True
+                else:
+                    return False
+            except:
+                return 'process intrupted'
+    else:
+        return ctypes.windll.shell32.IsUserAnAdmin()

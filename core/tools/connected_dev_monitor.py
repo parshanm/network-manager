5208929fd5c0651a0cf8ebe43e4130a40199c150
parshanm <parshan.m.m@gmail.com>
2026-01-31 15:57:54 +0330

    you can ping some ip but it has a bug I will fix it!!

--- core/tools/connected_dev_monitor.py
+++ core/tools/connected_dev_monitor.py
@@ -9,9 +9,24 @@ from ip import Ip
 """
 
 def get_connected(ip_range:list):
+    connected = []
+    available = []
     first = Ip(ip_range[0], ip_range[2])
     last = Ip(ip_range[1], ip_range[2])
     current = first
-    while current <= last:
-        pass
-    
+    while True:
+        command = f'ping {current.ip}'
+        a = os.popen(command).read()
+        if 'Sent' in a:
+            print(f'{current.ip} owend\n'+'-'*20)
+            connected.append(current.ip)
+        else:
+            print(f'{current.ip} is available\n'+'-'*20)
+            available.append(current.ip)
+        if current == last:
+            return connected, available
+        else:
+            current = current.next()
+            continue
+
+print(get_connected(['192.168.1.2', '192.168.1.30', 199]))
